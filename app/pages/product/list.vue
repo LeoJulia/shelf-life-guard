e38b23dfa1996cd8222e64ca27841c6e4e77260d
Julia Vu <mornemira@yandex.ru>
2026-06-01 17:15:25 +0300

    update navigation

--- app/pages/product/list.vue
+++ app/pages/product/list.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
 const { data } = await useFetch('/api/product-list');
-
-const onClickHandler = (id: string) => {
-  navigateTo(`/product/${id}`);
-}
 </script>
 
 <template>
   <div class="m-8" v-for="i in data">
-    <div @click="onClickHandler(i.id)">{{ i.brand }} - {{ i.productName }}</div>
+    <NuxtLink :to="{ name: 'product-id', params: { id: i.id } }">{{ i.brand }} - {{ i.productName }}</NuxtLink>
   </div>
 </template>
