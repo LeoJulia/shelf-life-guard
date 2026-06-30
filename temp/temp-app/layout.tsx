5b060e50f843e1b279ae5569fcb5cc0cb03b5f1a
Julia Vu <mornemira@yandex.ru>
2026-06-30 18:23:45 +0300

    temp

---
+++ temp/temp-app/layout.tsx
@@ -0,0 +1,34 @@
+import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Header } from "./_components";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang='en'
+      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
+    >
+      <body className='h-screen font-sans antialiased bg-background'>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
